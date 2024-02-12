5ec510b8b4fdc6064e6981ec7045be13a9f534af
望月翔 <mochidzukishou@mochidzukishounoMacBook-Pro.local>
2024-02-13 08:16:42 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,6 @@ import EditTask from "./pages/tasks/edit";
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <AntdApp>
           <DevtoolsProvider>
